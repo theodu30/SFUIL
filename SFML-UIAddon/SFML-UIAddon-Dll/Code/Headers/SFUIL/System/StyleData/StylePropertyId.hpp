8e64797e59fa4160e2f54247cf13c15affa9fdc0
Arca Théo <theo.arca@gmail.com>
2026-01-14 16:50:23 +0100

    Add StylePropertyId enum for style properties
    Introduced StylePropertyId.hpp defining an enum for various UI style properties. This enum exists in order to manipulate all properties through functions.

---
+++ SFML-UIAddon/SFML-UIAddon-Dll/Code/Headers/SFUIL/System/StyleData/StylePropertyId.hpp
@@ -0,0 +1,73 @@
+#pragma once
+
+enum class StylePropertyId
+{
+	Unknown = 0,
+
+	Opacity = 100,
+	Display = 101,
+	Visibility = 102,
+
+	Position = 200,
+	Top = 201,
+	Right = 202,
+	Bottom = 203,
+	Left = 204,
+
+	FlexBasic = 300,
+	FlexShrink = 301,
+	FlexGrow = 302,
+	FlexDirection = 303,
+	FlexWrap = 304,
+
+	AlignItems = 400,
+	JustifyContent = 401,
+	AlignSelf = 402,
+	AlignContent = 403,
+
+	Width = 500,
+	Height = 501,
+	MinWidth = 502,
+	MinHeight = 503,
+	MaxWidth = 504,
+	MinWidth = 505,
+	AspectRatio = 506,
+
+	Margin = 600,
+	MarginTop = 601,
+	MarginRight = 602,
+	MarginBottom = 603,
+	MarginLeft = 604,
+	Padding = 605,
+	PaddingTop = 606,
+	PaddingRight = 607,
+	PaddingBottom = 608,
+	PaddingLeft = 609,
+
+	TextFont = 700,
+	FontStyle = 701,
+	FontSize = 702,
+	TextColor = 703,
+	TextAlign = 704,
+
+	BackgroundColor = 800,
+	BackgroundImage = 801,
+	BackgroundTint = 802,
+	BackgroundRepeat = 803,
+	BackgroundWidth = 804,
+	BackgroundHeight = 805,
+	BackgroundPositionX = 806,
+	BackgroundScaleMode = 807,
+
+	BorderColor = 900,
+	BorderWidth = 901,
+	BorderHeight = 902,
+
+	OriginX = 1000,
+	OriginY = 1001,
+	TranslateX = 1002,
+	TranslateY = 1003,
+	ScaleX = 1004,
+	ScaleY = 1005,
+	Rotate = 1006
+};
